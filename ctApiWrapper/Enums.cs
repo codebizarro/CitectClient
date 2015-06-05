f757e9b3f9ee2baa2d389404c6ba8765a53a4b7f
Rudenko.A. <Rudenko.A.@localhost>
2015-06-05 11:42:23 +0300

    ?????? ????? ????? ?????? ? ?????????, ?????????? ?????????? ???????? ?? ?????????? ??????????? ?? ???? ???????

--- ctApiWrapper/Enums.cs
+++ ctApiWrapper/Enums.cs
@@ -140,4 +140,20 @@ Cluster - Clusters
         public const int DBTYPE_DBTIME = 134;
         public const int DBTYPE_DBTIMESTAMP = 135;
     };
+
+    [Flags]
+    public enum Quality
+    {
+        None = 0x1,
+        Interpolated = 0x2,
+        SingleRaw = 0x4,
+        MultipleRaw = 0x8,
+        Bad = 0x10,
+        Good = 0x20,
+        LastBad = 0x100,
+        LastGood = 0x200,
+        NotAvailable = 0x400,
+        Gated = 0x800,
+        Changed = 0x1000
+    }
 }
