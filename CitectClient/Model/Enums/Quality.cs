c1b1f33676fbf591df5eeb6ce7f241d239e24c38
Rudenko.A.E. <Rudenko.A.E.@localhost>
2018-06-04 17:42:49 +0300

    Renaming

---
+++ CitectClient/Model/Enums/Quality.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ctApiWrapper
+{
+    [Flags]
+    public enum Quality
+    {
+        None = 0x1,
+        Interpolated = 0x2,
+        SingleRaw = 0x4,
+        MultipleRaw = 0x8,
+        Bad = 0x10,
+        Good = 0x20,
+        LastBad = 0x100,
+        LastGood = 0x200,
+        NotAvailable = 0x400,
+        Gated = 0x800,
+        Changed = 0x1000
+    }
+}
